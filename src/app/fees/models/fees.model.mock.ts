d0f65c2afa49e184903848132058adfb8edb9043
Sam Ali <sam.alimirsalari@gmail.com>
2023-02-04 18:50:32 -0500

    add angular playground

--- src/app/fees/models/fees.model.mock.ts
+++ src/app/fees/models/fees.model.mock.ts
@@ -4,24 +4,44 @@ export const MOCK_FEE_ITEM_1: FeeModel = {
   id: 'item_1',
   fee: 0,
   additionalFee: null,
+  scheduledStartDate: new Date(),
+  tax: {
+    name: 'hst',
+    percent: 2,
+  },
 };
 
 export const MOCK_NEW_FEE_ITEM_1: FeeModel = {
   id: 'item_1',
   fee: 2.4,
   additionalFee: 2,
+  scheduledStartDate: null,
+  tax: {
+    name: 'gst',
+    percent: 2,
+  },
 };
 
 export const MOCK_FEE_ITEM_2: FeeModel = {
   id: 'item_2',
   fee: 2.24,
   additionalFee: null,
+  scheduledStartDate: null,
+  tax: {
+    name: 'gst',
+    percent: 4,
+  },
 };
 
 export const MOCK_FEE_ITEM_4: FeeModel = {
   id: 'item_4',
   fee: 6,
   additionalFee: 6,
+  scheduledStartDate: null,
+  tax: {
+    name: 'hst',
+    percent: 22,
+  },
 };
 
 export const MOCK_EXPECTED_FEES_MAPPING: Record<string, FeeModel> = {
--- src/app/fees/models/fees.model.ts
+++ src/app/fees/models/fees.model.ts
@@ -2,6 +2,13 @@ export type FeeModel = {
   id: string;
   fee: number;
   additionalFee: number | null;
+  scheduledStartDate: Date | null;
+  tax: Tax;
+};
+
+type Tax = {
+  name: 'hst' | 'gst';
+  percent: number;
 };
 
 export type FeesStateModel = {map: Record<string, FeeModel>};
